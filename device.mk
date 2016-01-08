aa08a7864de3d7b6675ed15486218e80f3f79080
Rob Herring <robh@kernel.org>
2016-01-08 16:22:26 -0600

    Initial commit
    This is a partially generated device configuration allowing building
    same config on multiple architectures. Since the Android build system
    uses TARGET_PRODUCT to set the build path and requires it match a
    directory under device/, this setup allows trivially adding
    architectures or other configuration options.
    
    Signed-off-by: Rob Herring <robh@kernel.org>

---
+++ device.mk
@@ -0,0 +1,34 @@
+#
+# Copyright (C) 2014 The Android Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+PRODUCT_NAME := $(TARGET_PRODUCT)
+PRODUCT_DEVICE := $(TARGET_PRODUCT)
+PRODUCT_BRAND := Android
+PRODUCT_MODEL := AOSP
+
+PRODUCT_PACKAGES += \
+    libGLES_mesa \
+    libGLES_android \
+    hwcomposer.drm \
+    gralloc.drm
+
+PRODUCT_COPY_FILES += $(call add-to-product-copy-files-if-exists,\
+			system/core/rootdir/init.rc:root/init.rc \
+			$(LOCAL_PATH)/init.rc:root/init.unknown.rc \
+			$(LOCAL_PATH)/fstab:root/fstab.unknown)
---
+++ vendorsetup.sh
@@ -0,0 +1,47 @@
+#
+# Copyright (C) 2013 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# This file is executed by build/envsetup.sh, and can use anything
+# defined in envsetup.sh.
+#
+# In particular, you can add lunch options with the add_lunch_combo
+# function: add_lunch_combo generic-eng
+
+# Note: all variables must be local or they get sourced.
+
+create_devices() {
+	local a
+	local arches="arm64 x86_64"
+	local config_base="linaro"
+	local device_dir="$(dirname "${BASH_SOURCE[0]}")"
+
+	for a in $arches; do
+		local config_name="${config_base}_${a}"
+		mkdir -p "${device_dir}/${config_name}"
+
+		cat << EOF > ${device_dir}/${config_name}/AndroidProducts.mk
+PRODUCT_MAKEFILES := ${config_name}:\$(LOCAL_DIR)/../device.mk
+EOF
+
+		cat << EOF > ${device_dir}/${config_name}/BoardConfig.mk
+include \$(SRC_TARGET_DIR)/board/generic_${a}/BoardConfig.mk
+include ${device_dir}/BoardConfig.mk
+EOF
+
+		add_lunch_combo ${config_name}-userdebug
+	done
+}
+create_devices
