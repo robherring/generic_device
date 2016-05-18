6e0d640022f62c48bd936aea20c0573ebffbb9b8
Rob Herring <robh@kernel.org>
2016-05-18 15:56:07 -0500

    Add sensor HAL support
    Signed-off-by: Rob Herring <robh@kernel.org>

---
+++ sensor/device.mk
@@ -0,0 +1,9 @@
+PRODUCT_PACKAGES += sensor.rc \
+    sensors.iio \
+    activity_recognition.iio \
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.sensor.barometer.xml:system/etc/permissions/android.hardware.sensor.barometer.xml \
+    frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
+    frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+    frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
