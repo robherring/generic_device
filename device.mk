e36fa275f6ad0a13fd24e05e9475cc2321f7708a
Rob Herring <robh@kernel.org>
2016-05-18 08:04:01 -0500

    Add some additional feature permission files
    Signed-off-by: Rob Herring <robh@kernel.org>

--- device.mk
+++ device.mk
@@ -44,8 +44,10 @@ PRODUCT_COPY_FILES += $(call add-to-product-copy-files-if-exists,\
 )
 
 PRODUCT_COPY_FILES += \
-	frameworks/native/data/etc/android.hardware.screen.landscape.xml:system/etc/permissions/android.hardware.screen.landscape.xml \
-	frameworks/native/data/etc/android.hardware.screen.portrait.xml:system/etc/permissions/android.hardware.screen.portrait.xml
+    frameworks/native/data/etc/android.hardware.screen.landscape.xml:system/etc/permissions/android.hardware.screen.landscape.xml \
+    frameworks/native/data/etc/android.hardware.screen.portrait.xml:system/etc/permissions/android.hardware.screen.portrait.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
 
 include $(wildcard $(LOCAL_PATH)/*/device.mk)
 
