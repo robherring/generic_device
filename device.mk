55d4bfc38e85f8c9777c12a96ffb881096dc6d25
Amit Pundir <amit.pundir@linaro.org>
2016-07-26 17:25:57 +0530

    linaro_generic: Add generic configfs usb gadget support
    Only ADB, MTP/PTP and RNDIS functions are supported at the moment.
    
    Pick USB gadget configuration from /system/etc/init/gadgets.rc at run
    time.
    
    Signed-off-by: Amit Pundir <amit.pundir@linaro.org>
    Signed-off-by: Rob Herring <robh@kernel.org>

--- device.mk
+++ device.mk
@@ -56,6 +56,9 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
     $(LOCAL_PATH)/media_codecs.xml:system/etc/media_codecs.xml \
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/gadgets.rc:system/etc/init/gadgets.rc
+
 subdirs-true :=
 subdirs-$(CONFIG_WIFI) += wifi
 subdirs-$(CONFIG_ETHERNET) += ethernet
