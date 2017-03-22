852d0412308c37d79a958a2f47eb7c33ecb36177
Rob Herring <robh@kernel.org>
2017-03-22 11:50:12 -0500

    graphics: switch from gralloc.drm to gralloc.gbm
    We've changed the name, so update the device to build and pickup the
    right gralloc.
    
    Signed-off-by: Rob Herring <robh@kernel.org>

--- graphics/drm/device.mk
+++ graphics/drm/device.mk
@@ -3,7 +3,7 @@ PRODUCT_PACKAGES += drm.rc
 PRODUCT_PACKAGES += \
 		libGLES_mesa \
 		hwcomposer.drm \
-		gralloc.drm
+		gralloc.gbm
 
 BOARD_SEPOLICY_DIRS += \
 	device/linaro/generic/graphics/drm/sepolicy
