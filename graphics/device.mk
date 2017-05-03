dbe4e5fe0dc6f105ba82c6baeae4ef5fb17a91b5
John Stultz <john.stultz@linaro.org>
2017-05-02 20:52:08 -0700

    Add DPI override option
    This patch adds DPI override option and sets the db410c to 160.
    If the value is left at the default 0, it will be probed from
    the monitor.
    
    Cc: Rob Herring <rob.herring@linaro.org>
    Cc: Sumit Semwal <sumit.semwal@linaro.org>
    Cc: Vishal Bhoj <vishal.bhoj@linaro.org>
    Cc: Amit Pundir <amit.pundir@linaro.org>
    Signed-off-by: John Stultz <john.stultz@linaro.org>
    Signed-off-by: Rob Herring <robh@kernel.org>

--- graphics/device.mk
+++ graphics/device.mk
@@ -3,6 +3,9 @@ PRODUCT_PROPERTY_OVERRIDES := $(if $(CONFIG_SW_GRAPHICS), \
 
 PRODUCT_PACKAGES := libGLES_android
 
+PRODUCT_PROPERTY_OVERRIDES += \
+        ro.sf.lcd_density=$(CONFIG_DISPLAY_DPI)
+
 subdirs-true :=
 subdirs-$(BOARD_USES_DRM_HWCOMPOSER) += drm
 subdirs-$(CONFIG_GRALLOC_MALI) += gralloc
