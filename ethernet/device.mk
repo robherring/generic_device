d6d1ed1ff5de28eda704c4b26e03bd09b3d3910a
Rob Herring <robh@kernel.org>
2017-08-11 15:25:52 -0500

    ethernet: remove dhcpcd service
    dhcpcd is no longer needed for ethernet DHCP as Android's built-in DHCP
    server is used, so remove the related files.
    
    Signed-off-by: Rob Herring <robh@kernel.org>

--- ethernet/device.mk
+++ ethernet/device.mk
@@ -1,5 +1,2 @@
-PRODUCT_PACKAGES := ethernet.rc
-
 PRODUCT_COPY_FILES := \
 	frameworks/native/data/etc/android.hardware.ethernet.xml:system/etc/permissions/android.hardware.ethernet.xml \
-        $(LOCAL_PATH)/dhcpcd.conf:system/etc/dhcpcd/dhcpcd.conf \
