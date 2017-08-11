31472abb4e94a51d8435b01d87377f118b2ac1f4
Rob Herring <robh@kernel.org>
2017-08-11 15:35:30 -0500

    Add a vendor manifest
    This doesn't seem to matter functionally ATM, but quiets down
    hwservicemanager errors.
    
    Signed-off-by: Rob Herring <robh@kernel.org>

--- device.mk
+++ device.mk
@@ -62,6 +62,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
     frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
     $(LOCAL_PATH)/media_codecs.xml:system/etc/media_codecs.xml \
+    $(LOCAL_PATH)/vintf.xml:system/vendor/manifest.xml \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gadgets.rc:system/etc/init/gadgets.rc \
