33fde31069173b18f4bca2ae49d1e6996948dc86
Rob Herring <robh@kernel.org>
2017-09-15 14:20:00 -0500

    wifi: ti: move firmware to vendor path
    Signed-off-by: Rob Herring <robh@kernel.org>

--- wifi/ti_wilink/device.mk
+++ wifi/ti_wilink/device.mk
@@ -1,4 +1,4 @@
 PRODUCT_COPY_FILES := \
-	$(LOCAL_PATH)/TIInit_11.8.32.bts:system/etc/firmware/ti-connectivity/TIInit_11.8.32.bts \
-	$(LOCAL_PATH)/wl18xx-fw-4.bin:system/etc/firmware/ti-connectivity/wl18xx-fw-4.bin \
-	$(LOCAL_PATH)/wl18xx-conf.bin:system/etc/firmware/ti-connectivity/wl18xx-conf.bin
+	$(LOCAL_PATH)/TIInit_11.8.32.bts:$(TARGET_OUT_VENDOR)/etc/firmware/ti-connectivity/TIInit_11.8.32.bts \
+	$(LOCAL_PATH)/wl18xx-fw-4.bin:$(TARGET_OUT_VENDOR)/etc/firmware/ti-connectivity/wl18xx-fw-4.bin \
+	$(LOCAL_PATH)/wl18xx-conf.bin:$(TARGET_OUT_VENDOR)/etc/firmware/ti-connectivity/wl18xx-conf.bin
