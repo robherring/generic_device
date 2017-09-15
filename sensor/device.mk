059672777b6e3c065af28bbb3c1d940aab85f778
Rob Herring <robh@kernel.org>
2017-09-15 14:17:41 -0500

    sensor: move sensor.rc to vendor path
    As part of Treble project in Android O, all the device specific files have
    to be located in a separate vendor partition. This is done by setting
    LOCAL_PROPRIETARY_MODULE (the name is misleading). This change will not
    break existing platforms without a vendor partition as it will just move
    files to /system/vendor.
    
    Signed-off-by: Rob Herring <robh@kernel.org>

--- sensor/device.mk
+++ sensor/device.mk
@@ -1,9 +1,9 @@
 PRODUCT_PACKAGES := \
-    sensor.rc \
     sensors.iio \
     activity_recognition.iio \
 
 PRODUCT_COPY_FILES := \
+    $(LOCAL_PATH)/sensor.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/sensor.rc \
     frameworks/native/data/etc/android.hardware.sensor.barometer.xml:system/etc/permissions/android.hardware.sensor.barometer.xml \
     frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
     frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
