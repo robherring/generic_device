44c5aa45c93bec5122a4f638ad5f978fed64bd34
Rob Herring <robh@kernel.org>
2017-09-15 11:17:31 -0500

    lowmem: Move lowmem.rc to vendor path
    As part of Treble project in Android O, all the device specific files have
    to be located in a separate vendor partition. Use TARGET_COPY_OUT_VENDOR
    which will be set correctly based on split vendor partition or not.
    
    Phony packages don't work for /vendor so just copy the file instead.
    
    Signed-off-by: Rob Herring <robh@kernel.org>

--- lowmem/device.mk
+++ lowmem/device.mk
@@ -1,8 +1,6 @@
-#lowmem items
-PRODUCT_PACKAGES := \
-		lowmem.rc \
-
 PRODUCT_PROPERTY_OVERRIDES += \
 	ro.config.low_ram=true \
 	dalvik.vm.jit.codecachesize=0 \
 
+PRODUCT_COPY_FILES := \
+	$(LOCAL_PATH)/lowmem.rc:$(TARGET_OUT_VENDOR)/etc/init/lowmem.rc
