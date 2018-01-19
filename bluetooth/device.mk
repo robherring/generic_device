6170292e3cac0c25de7cf54c8d6481c3797fc34a
Amit Pundir <amit.pundir@linaro.org>
2018-01-19 20:23:19 +0530

    db410c-userdebug: Enable Bluetooth
    Verified with standard bluetooth mouse.
    
    Signed-off-by: Amit Pundir <amit.pundir@linaro.org>
    Signed-off-by: Rob Herring <robh@kernel.org>

--- bluetooth/device.mk
+++ bluetooth/device.mk
@@ -1,1 +1,5 @@
 PRODUCT_PACKAGES := android.hardware.bluetooth@1.0-service.btlinux
+
+PRODUCT_COPY_FILES += \
+	frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml \
+	frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml
